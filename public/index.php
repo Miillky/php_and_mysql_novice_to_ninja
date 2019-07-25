6ceba0a8a22dcbeff331b29e3dc46d07ad1d3759
Miillky <vedran.milkovic25@gmail.com>
2019-07-25 19:51:23 +0200

    Routing engine for controllers and .htaccess

--- public/index.php
+++ public/index.php
@@ -18,16 +18,64 @@ try {
 
 	include __DIR__ . '/../includes/DatabaseConnection.php';
 	include __DIR__ . '/../classes/DatabaseTable.php';
-	include __DIR__ . '/../controllers/JokeController.php';
 
-	$jokesTable   = new DatabaseTable( $pdo, 'joke', 'id' );
-	$authorsTable = new DatabaseTable( $pdo, 'author', 'id' );
-
-	$jokeController = new JokeController( $jokesTable, $authorsTable );
+	$jokesTable   		= new DatabaseTable( $pdo, 'joke', 'id' );
+	$authorsTable 		= new DatabaseTable( $pdo, 'author', 'id' );
 
 	$action = $_GET['action'] ?? 'home';
 
-	$page 	= $jokeController->$action();
+	$route = ltrim( strtok( $_SERVER['REQUEST_URI'], '?' ), '/' );
+
+	if( $route == strtolower( $route ) ){
+
+		if( $route === 'joke/list' ){
+
+			include __DIR__ . '/../controllers/JokeController.php';
+
+			$controller = new JokeController( $jokesTable, $authorsTable );
+
+			$page = $controller->list();
+
+		} elseif ( $route === 'joke/home' ){
+
+			include __DIR__ . '/../controllers/JokeController.php';
+
+			$controller = new JokeController( $jokesTable, $authorsTable );
+
+			$page = $controller->home();
+
+		} elseif( $route === 'joke/edit' ){
+
+			include __DIR__ . '/../controllers/JokeController.php';
+
+			$controller = new JokeController( $jokesTable, $authorsTable );
+
+			$page = $controller->edit();
+
+		} elseif( $route === 'joke/delete' ){
+
+			include __DIR__ . '/../controllers/JokeController.php';
+
+			$controller = new JokeController( $jokesTable, $authorsTable );
+
+			$page = $controller->delete();
+
+		} elseif( $route === 'register' ){
+
+			include __DIR__ . '/../controllers/RegisterController.php';
+
+			$controller = new RegisterController( $authorsTable );
+
+			$page = $controller->showForm();
+
+		}
+
+	} else {
+
+		http_response_code(301);
+		header( 'location: index.php?route=' . strtolower( $route ) );
+
+	}
 
 	$title 	= $page['title'];
 
--- templates/layout.html.php
+++ templates/layout.html.php
@@ -14,8 +14,8 @@
 	<nav>
 		<ul>
 			<li><a href="index.php">Home</a></li>
-			<li><a href="index.php?action=list">Jokes List</a></li>
-			<li><a href="index.php?action=edit">Add a new Joke</a></li>
+			<li><a href="index.php?route=joke/list">Jokes List</a></li>
+			<li><a href="index.php?action=joke/edit">Add a new Joke</a></li>
 		</ul>
 	</nav>
 	<main>
