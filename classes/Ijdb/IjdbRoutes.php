e2dce19ecbf5f418039c815dbceaa2703b19bcfb
Miillky <vedran.milkovic25@gmail.com>
2019-07-29 23:46:39 +0200

    User registration, registration validation, password hashing

--- classes/Ijdb/IjdbRoutes.php
+++ classes/Ijdb/IjdbRoutes.php
@@ -11,36 +11,53 @@ class IjdbRoutes implements \Ninja\Routes {
 		$jokesTable   = new \Ninja\DatabaseTable( $pdo, 'joke', 'id' );
 		$authorsTable = new \Ninja\DatabaseTable( $pdo, 'author', 'id' );
 
-		$jokeController = new \Ijdb\Controllers\Joke( $jokesTable, $authorsTable );
+		$jokeController   = new \Ijdb\Controllers\Joke( $jokesTable, $authorsTable );
+		$authorController = new \Ijdb\Controllers\Register( $authorsTable );
 
 		$routes = [
-					'joke/edit' => [
-										'POST' => [
-													'controller' => $jokeController,
-													'action' => 'saveEdit'
-												  ],
-									   'GET'   => [
-													'controller' => $jokeController,
-													'action' => 'edit'
-									   			  ]
-								   ],
-					'joke/delete' => [
-										'POST' => [
-													'controller' => $jokeController,
-													'action'	 => 'delete'
-												  ]
-									 ],
+					'author/register' => [
+											'GET'  => [
+														'controller' => $authorController,
+														'action'	 => 'registrationForm'
+													  ],
+											'POST' => [
+														'controller' => $authorController,
+														'action'	 => 'registerUser'
+													  ]
+									     ],
+					'author/success'  => [
+											'GET' => [
+														'controller' => $authorController,
+														'action'	 => 'success'
+													 ]
+										],
+					'joke/edit'   	  => [
+											'POST' => [
+														'controller' => $jokeController,
+														'action' 	 => 'saveEdit'
+													  ],
+											'GET'  => [
+															'controller' => $jokeController,
+															'action' 	 => 'edit'
+													  ]
+										 ],
+					'joke/delete' 	  => [
+											'POST' => [
+														'controller' => $jokeController,
+														'action'	 => 'delete'
+													  ]
+										 ],
 					'joke/list'   => [
-										'GET'  => [
-													'controller' => $jokeController,
-													'action'	 => 'list'
-												  ]
+										'GET'  	   => [
+														'controller' => $jokeController,
+														'action'	 => 'list'
+													  ]
 									 ],
 					''			  => [
-										'GET' => [
-													'controller' => $jokeController,
-													'action'	 => 'home'
-												 ]
+										'GET' 	   => [
+														'controller' => $jokeController,
+														'action'	 => 'home'
+													  ]
 									 ]
 				  ];
 
--- classes/Ninja/DatabaseTable.php
+++ classes/Ninja/DatabaseTable.php
@@ -45,13 +45,23 @@ class DatabaseTable {
 
 		$query = 'SELECT * FROM `' . $this->table . '` WHERE `' . $this->primaryKey . '` = :value';
 
-		$parameters = [
-			'value' => $value
-		];
+		$parameters = [ 'value' => $value ];
 
 		$query = $this->query( $query, $parameters );
 
 		return $query->fetch();
+
+	}
+
+	public function find( $column, $value ){
+
+		$query = 'SELECT * FROM ' . $this->table . ' WHERE ' . $column . ' =:value';
+
+		$parameters = [ 'value' => $value ];
+
+		$query = $this->query( $query, $parameters );
+
+		return $query->fetchAll();
 
 	}
 
